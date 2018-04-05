fc931bf73623c3655e4175b9d710406b05d913cf
Daryl Guieb <dgu@hq.ivalua.com>
2018-04-05 15:42:33 -0700

    added semantic, jquery

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// require('../node_modules/semantic-ui/dist/semantic.min.js')
+require('../node_modules/jquery/dist/jquery.min.js')
+
+require('./styles/main.css')
+require('../node_modules/semantic-ui/dist/semantic.min.css')
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -11,5 +17,6 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  data: {message: 'Hello world'}
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
+import Page from '@/components/Page'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
+    },
+    {
+      path: '/page',
+      name: 'Page',
+      component: Page
     }
   ]
 })

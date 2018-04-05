5975edf48b69e818a46cc61705ddc196fae237bb
Daryl Guieb <dgu@hq.ivalua.com>
2018-04-05 15:42:44 -0700

    added files

---
+++ src/components/Page.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+  <div class="ui menu">
+    <div class="header item">
+      Our Company
+    </div>
+    <a class="item">
+      About Us
+    </a>
+    <a class="item">
+      Jobs
+    </a>
+    <a class="item">
+      Locations
+    </a>
+  </div>
+    <h1>{{ msg }}</h1>
+    <div class="ui container">
+      <div class="ui segment red">
+        <i class="american sign language interpreting icon"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page',
+  data () {
+    return {
+      msg: 'Page View'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
